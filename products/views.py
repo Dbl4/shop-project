f61c415d69e9f6b7a3bbef878f67b5e2fa8e16c3
Erofeev Vladislav <vlad27-27@mail.ru>
2021-05-29 18:39:25 +0300

    added placeholder index

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop',
+        'h1': 'GeekShop Store',
+        'p': 'Новые образы и лучшие бренды на GeekShop Store. Бесплатная доставка по всему миру! Аутлет: до -70% Собственный бренд. -20% новым покупателям.'
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
     return render(request, 'products/products.html')
-
