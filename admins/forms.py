9dfeb890c925070895168c9ab66cb44925e00a4f
Dbl4 <71012183+Dbl4@users.noreply.github.com>
2021-06-17 09:23:05 +0000

    added UPDATE for admin_user_update

--- admins/forms.py
+++ admins/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 
 from users.models import User
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserProfileForm
 
 class UserAdminRegisterForm(UserRegisterForm):
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'image', 'password1',
-                  'password2')
+        fields = ('username', 'email', 'first_name', 'last_name', 'image', 'password1', 'password2')
+
+class UserAdminProfileForm(UserProfileForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control py-4','readonly': False}))
+    email = forms.CharField(widget=forms.EmailInput(
+        attrs={'class': 'form-control py-4', 'readonly': False}))
--- admins/views.py
+++ admins/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from users.models import User
-from admins.forms import UserAdminRegisterForm
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 def index(request):
     return render(request, 'admins/admin.html')
@@ -22,8 +22,6 @@ def admin_users_create(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('admins:admin_users'))
-        else:
-            print(form.errors)
     else:
         form = UserAdminRegisterForm()
     context = {
@@ -34,7 +32,20 @@ def admin_users_create(request):
 
 
 def admin_users_update(request, id):
-    return render(request, 'admins/admin-users-update-delete.html')
+    selected_user = User.objects.get(id=id)
+    if request.method == "POST":
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=selected_user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+      'title': 'GeekShop - Админ | Обновление пользователя',
+      'form': form,
+      'selected_user': selected_user,
+    }
+    return render(request, 'admins/admin-users-update-delete.html', context)
 
 
 def admin_users_delete(request, id):
