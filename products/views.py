30ea5eb8cdf7ab85cb0202b58b48eaff80ac9708
Erofeev Vladislav <vlad27-27@mail.ru>
2021-06-20 00:36:56 +0300

    added admin_catigories_update,admin_catigories_update

---
+++ products/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from products.models import Product, ProductCategory
+
+
+def index(request):
+    context = {
+        'title': 'GeekShop',
+        'h1': 'GeekShop Store',
+        'p': 'Новые образы и лучшие бренды на GeekShop Store. Бесплатная доставка по всему миру! Аутлет: до -70% Собственный бренд. -20% новым покупателям.'
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'h1': 'GeekShop',
+        'footer_title': 'GeekShop',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()
+    }
+    return render(request, 'products/products.html', context)
