50bdc53ea76d4821495a41c73e1cfbb09cb9e72d
Dbl4 <71012183+Dbl4@users.noreply.github.com>
2021-06-23 11:39:20 +0000

    created CBV for admins

--- admins/urls.py
+++ admins/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
 
-from admins.views import index, UserCreateView, UserListView, UserUpdateView, UserDeleteView, \
-    admin_categories, admin_categories_create, admin_categories_update, admin_categories_delete
+from admins.views import UserTemplateView, UserCreateView, UserListView, UserUpdateView, UserDeleteView, \
+    CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
 
 app_name = 'admins'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', UserTemplateView.as_view(), name='index'),
     path('users/', UserListView.as_view(), name='admin_users'),
     path('users/create/', UserCreateView.as_view(), name='admin_users_create'),
     path('users/update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
     path('users/delete/<int:pk>/', UserDeleteView.as_view(), name='admin_users_delete'),
-    path('categories/', admin_categories, name='admin_categories'),
-    path('categories/create/', admin_categories_create, name='admin_categories_create'),
-    path('categories/update/<int:id>/', admin_categories_update, name='admin_categories_update'),
-    path('categories/delete/<int:id>/', admin_categories_delete, name='admin_categories_delete'),
+    path('categories/', CategoryListView.as_view(), name='admin_categories'),
+    path('categories/create/', CategoryCreateView.as_view(), name='admin_categories_create'),
+    path('categories/update/<int:pk>/', CategoryUpdateView.as_view(), name='admin_categories_update'),
+    path('categories/delete/<int:pk>/', CategoryDeleteView.as_view(), name='admin_categories_delete'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,26 +1,22 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
+from django.views.generic.base import TemplateView
 
 from users.models import User
 from admins.forms import UserAdminRegisterForm, UserAdminProfileForm,ProductCategoryAdminForm
 from products.models import ProductCategory
 
-@user_passes_test(lambda u: u.is_superuser)
-def index(request):
-    return render(request, 'admins/admin.html')
 
+class UserTemplateView(TemplateView):
+    template_name = 'admins/admin.html'
 
-# @user_passes_test(lambda u: u.is_superuser)
-# def admin_users(request):
-#     context = {
-#       'users': User.objects.all(),
-#       'title': 'GeekShop - Админ | Пользователи',
-#     }
-#     return render(request, 'admins/admin-users-read.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserTemplateView, self).dispatch(request, *args, **kwargs)
 
 
 class UserListView(ListView):
@@ -106,51 +102,79 @@ class UserDeleteView(DeleteView):
         return HttpResponseRedirect(self.get_success_url())
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_categories(request):
-    context = {
-      'categories': ProductCategory.objects.all(),
-      'title': 'GeekShop - Админ | Категории',
-    }
-    return render(request, 'admins/admin-categories-read.html', context)
+class CategoryListView(ListView):
+    model = ProductCategory
+    template_name = 'admins/admin-categories-read.html'
+    
+    def get_context_data(self, list_object=None, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - Админ | Категории'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(CategoryListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_categories_create(request):
-    if request.method == 'POST':
-        form = ProductCategoryAdminForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_categories'))
-    else:
-        form = ProductCategoryAdminForm()
-    context = {
-      'title': 'GeekShop - Админ | Создание категории',
-      'form': form,
-    }
-    return render(request, 'admins/admin-categories-create.html', context)
+class CategoryCreateView(CreateView):
+    model = ProductCategory
+    template_name = 'admins/admin-categories-create.html'
+    form_class = ProductCategoryAdminForm
+    success_url = reverse_lazy('admins:admin_categories')
+
+    def get_context_data(self, list_object=None, **kwargs):
+        context = super(CategoryCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - Админ | Создание категории'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(CategoryCreateView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_categories_update(request, id):
-    selected_category = ProductCategory.objects.get(id=id)
-    if request.method == "POST":
-        form = ProductCategoryAdminForm(data=request.POST, instance=selected_category)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_categories'))
-    else:
-        form = ProductCategoryAdminForm(instance=selected_category)
-    context = {
-      'title': 'GeekShop - Админ | Обновление пользователя',
-      'form': form,
-      'selected_category': selected_category,
-    }
-    return render(request, 'admins/admin-categories-update-delete.html', context)
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_categories_update(request, id):
+#     selected_category = ProductCategory.objects.get(id=id)
+#     if request.method == "POST":
+#         form = ProductCategoryAdminForm(data=request.POST, instance=selected_category)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_categories'))
+#     else:
+#         form = ProductCategoryAdminForm(instance=selected_category)
+#     context = {
+#       'title': 'GeekShop - Админ | Обновление пользователя',
+#       'form': form,
+#       'selected_category': selected_category,
+#     }
+#     return render(request, 'admins/admin-categories-update-delete.html', context)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_categories_delete(request, id):
-    category = ProductCategory.objects.get(id=id)
-    category.delete()
-    return HttpResponseRedirect(reverse('admins:admin_categories'))
+class CategoryUpdateView(UpdateView):
+    model = ProductCategory
+    template_name = 'admins/admin-categories-update-delete.html'
+    form_class = ProductCategoryAdminForm
+    success_url = reverse_lazy('admins:admin_categories')
+
+    def get_context_data(self, list_object=None, **kwargs):
+        context = super(CategoryUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - Админ | Обновление пользователя'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(CategoryUpdateView, self).dispatch(request, *args, **kwargs)
+
+
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_categories_delete(request, id):
+#     category = ProductCategory.objects.get(id=id)
+#     category.delete()
+#     return HttpResponseRedirect(reverse('admins:admin_categories'))
+
+class CategoryDeleteView(DeleteView):
+    model = ProductCategory
+    template_name = 'admins/admin-categories-update-delete.html'
+    form_class = ProductCategoryAdminForm
+    success_url = reverse_lazy('admins:admin_categories')
