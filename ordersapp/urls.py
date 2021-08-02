c2607208dacb25086d9b3716147e4f223cdb5b94
Erofeev Vladislav <vlad27-27@mail.ru>
2021-08-03 00:50:29 +0300

    Revert "addded CBV for ordersapp OrderList, OrderCreate, OrderUpdate, OrderDelete, OrderRead"
    This reverts commit 2920fd1f

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from .views import OrderList, OrderCreate, OrderRead, OrderDelete, OrderUpdate, order_forming_complete
+
+from products.views import ProductsListView
 
 app_name = 'ordersapp'
 
-urlpatterns = [
-    path('', OrderList.as_view(), name='orders_list'),
-    path('forming/complete/<int:pk>/', order_forming_complete, name='order_forming_complete'),
-    path('create/', OrderCreate.as_view(), name='order_create'),
-    path('read/<int:pk>/', OrderRead.as_view(), name='order_read'),
-    path('update/<int:pk>/', OrderUpdate.as_view(), name='order_update'),
-    path('delete/<int:pk>/', OrderDelete.as_view(), name='order_delete'),
-]
+# urlpatterns = [
+#     path('', ProductsListView.as_view(), name = 'index'),
+#     path('<int:category_id>/', ProductsListView.as_view(), name = 'product'),
+#     path('page/<int:page>/', ProductsListView.as_view(), name = 'page'),
+# ]
